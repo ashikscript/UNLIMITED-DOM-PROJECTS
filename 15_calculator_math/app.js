0da59228d8db1bee90c55d8a6d64dc724216b715
Ashik <xashikx170@gmail.com>
2024-10-17 22:16:47 +0600

    new project upload

---
+++ 15_calculator_math/app.js
@@ -0,0 +1,15 @@
+const userInput = document.getElementById("userInput");
+var expression = "";
+
+function press(num) {
+  expression += num;
+  userInput.value = expression;
+}
+function equal() {
+  userInput.value = eval(expression);
+  expression = "";
+}
+function erase() {
+  expression = "";
+  userInput.value = expression;
+}
