cdc8d60f265ff452b54940e8dfa899e509d0fd27
Ashik <xashikx170@gmail.com>
2024-10-24 10:33:16 +0600

    toggle button dark mode

---
+++ 22_darkmode_toggle_bar/app.js
@@ -0,0 +1,9 @@
+let toggler = document.getElementById("switch");
+
+toggler.addEventListener("click", () => {
+  if (toggler.checked === true) {
+    document.body.style.backgroundColor = "black";
+  } else {
+    document.body.style.backgroundColor = "white";
+  }
+});
