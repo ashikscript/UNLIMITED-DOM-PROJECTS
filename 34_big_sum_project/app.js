fb183809538a0051dd38815e5d65eaa7f352447b
Ashik <xashikx170@gmail.com>
2024-11-05 10:21:01 +0600

    2 project uplod

---
+++ 34_big_sum_project/app.js
@@ -0,0 +1,37 @@
+var number1 = parseInt(prompt("Enter your fast number : "));
+var number2 = parseInt(prompt("Enter your fast number : "));
+var number3 = parseInt(prompt("Enter your fast number : "));
+var number4 = parseInt(prompt("Enter your fast number : "));
+var number5 = parseInt(prompt("Enter your fast number : "));
+var number6 = parseInt(prompt("Enter your fast number : "));
+var number7 = parseInt(prompt("Enter your fast number : "));
+var number8 = parseInt(prompt("Enter your fast number : "));
+var number9 = parseInt(prompt("Enter your fast number : "));
+var number10 = parseInt(prompt("Enter your fast number : "));
+var number11 = parseInt(prompt("Enter your fast number : "));
+var number12 = parseInt(prompt("Enter your fast number : "));
+var number13 = parseInt(prompt("Enter your fast number : "));
+var number14 = parseInt(prompt("Enter your fast number : "));
+
+var total =
+  number1 +
+  number2 +
+  number3 +
+  number4 +
+  number5 +
+  number6 +
+  number7 +
+  number8 +
+  number9 +
+  number10 +
+  number11 +
+  number12 +
+  number13 +
+  number14;
+
+console.log("Total payment = " + total);
+
+// Answer
+// Total payment = 116200
+
+//total 14 number
